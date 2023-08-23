4e3560409493e83ee6691590de536c650fb0f6b6
13-LOHITHA <121375227+13-LOHITHA@users.noreply.github.com>
2023-08-23 20:00:09 +0530

    Add files via upload

---
+++ Application5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.application5;
+
+/**
+ *
+ * @author lohit
+ */
+public class Application5 {
+
+    public static void main(String[] args) {
+        int days = 365; // Change this to the desired number of days
+        double speedOfLight = 299792458.0; // Speed of light in meters per second
+
+        double distance = speedOfLight * days * 24 * 60 * 60; // Distance = speed * time
+
+        System.out.println("Distance traveled by light in " + days + " days is " + distance + " meters.");
+    }
+}
+    
+        
